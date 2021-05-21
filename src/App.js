f2ded3d9ebbca35cb1ed33b66189a441946cd133
Sundar Raj sharma <50244255+sundarsharma332@users.noreply.github.com>
2021-05-21 22:44:39 +0530

    set up the env and started working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import image from './public_images/react.png';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>Qspace : Your Own personalized Space</h1>
+
+    <h2>Todos</h2>
+
+    <ul>
+      <li>User Login with Email and Password</li>
+      <li>Personalized News Feed with Shorts</li>
+      <li>Mini Social Media Appliation</li>
+      <li>Expense Management System and Alert</li>
+      <li>Fitness Montering and Activities</li>
+      <li>Personalized NoteBook and SketchPad</li>
+      <li>Top Products from all Ecommerce websites</li>
+      <li>Unlimited Learning Resources</li>
+
+    </ul>
+
+    <img className="img__banner" src={image} alt=""/>
     </div>
   );
 }
